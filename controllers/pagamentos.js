5894431bf2dc27f9a9dfa68d0b35f19d92f7b7c9
Marcio Mendes <mmendes.as@gmail.com>
2017-05-25 19:44:39 -0300

    funcionalidade basica da api de pagamentos

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,7 +1,44 @@
 
 module.exports = function (app) {
+
     app.get('/pagamentos', function (req, res) {
         console.log('Recebida requisição de teste.');
         res.send('OK');
+    });
+
+    app.post('/pagamentos/pagamento', function (req, res) {
+
+        req.assert("forma_de_pagamento", "Forma de pagamento eh obrigatorio").notEmpty();
+        req.assert("valor", "Valor eh obrigatorio e deve ser um decimal").notEmpty().isFloat();
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            console.log('Erros de validacao encontrados');
+            res.status(400).send(erros);
+            return;
+        }
+
+        var pagamento = req.body;
+        console.log('processando uma requisicao de um novo pagamento');
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.salva(pagamento, function (erro, resultado) {
+
+            if (erro) {
+                console.log('Erro ao inserir no banco: ' + erro);
+                res.status(500).send(erro);
+            } else {
+                console.log('pagamento criado');
+                res.location('/pagamentos/pagamento/' + resultado.insertId);
+                res.status(201).json(pagamento);
+            }
+        });
+
     });
 }
